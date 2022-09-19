bfa9255cc8b0c4fb8d6d6117f5f293d54e4a4eac
doraaan89 <doraaan89@gmail.com>
2022-09-20 00:35:47 +0900

    init

---
+++ script/layout.js
@@ -0,0 +1,26 @@
+export const header = {
+  el: '#header',
+  data() {
+    return {
+      headerText: 'header 영역이다',
+    }
+  },
+}
+
+export const footer = {
+  el: '#footer',
+  data() {
+    return {
+      footerText: 'footer 영역이다',
+    }
+  }
+}
+
+export const rnb = {
+  el: '#rnb',
+  data() {
+    return {
+      rnbText: 'rnb 영역이다.'
+    }
+  }
+}
---
+++ script/script.js
@@ -0,0 +1,6 @@
+import { header, footer, rnb } from './layout.js'
+
+// layout
+new Vue(header);
+new Vue(footer);
+new Vue(rnb);
